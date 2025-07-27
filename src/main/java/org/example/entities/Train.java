fd8856d0ab8d7b0730e1bc5ffd47175e073fb8de
Sachin Parmar <parmarsachin707@gmail.com>
2025-07-27 11:19:03 +0530

    Get & Set Method Setup

---
+++ src/main/java/org/example/entities/Train.java
@@ -0,0 +1,69 @@
+package org.example.entities;
+
+import java.sql.Time;
+import java.util.List;
+import java.util.Map;
+
+public class Train {
+
+    private String trainId;
+
+    private String trainNo;
+
+    private List<List<Integer>> seats;
+
+    private Map<String, Time> stationTimes;
+
+    private List<String> stations;
+
+    public Train(String trainId, String trainNo, List<List<Integer>> seats, Map<String, Time> stationTimes, List<String> stations) {
+
+        this.trainId = trainId;
+        this.trainNo = trainNo;
+        this.seats = seats;
+        this.stationTimes = stationTimes;
+        this.stations = stations;
+
+    }
+
+    public String getTrainId() {
+        return trainId;
+    }
+
+    public String getTrainNo() {
+        return trainNo;
+    }
+
+    public List<List<Integer>> getSeats() {
+        return seats;
+    }
+
+    public List<String> getStations() {
+        return stations;
+    }
+
+    public Map<String, Time> getStationTimes() {
+        return stationTimes;
+    }
+
+    public void setTrainId(String trainId) {
+        this.trainId = trainId;
+    }
+
+    public void setTrainNo(String trainNo) {
+        this.trainNo = trainNo;
+    }
+
+    public void setSeats(List<List<Integer>> seats) {
+        this.seats = seats;
+    }
+
+    public void setStations(List<String> stations) {
+        this.stations = stations;
+    }
+
+    public void setStationTimes(Map<String, Time> stationTimes) {
+        this.stationTimes = stationTimes;
+    }
+
+}
---
+++ src/main/java/org/example/services/UserBookingService.java
@@ -0,0 +1,30 @@
+package org.example.services;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.entities.User;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class UserBookingService {
+
+    private User user;
+
+    private static final String USER_PATH = "../localDB/users.json";
+
+    private List<User> userList;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    public UserBookingService(User user) throws IOException {
+
+        this.user = user;
+        File users = new File(USER_PATH);
+        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {});
+
+    }
+
+}
